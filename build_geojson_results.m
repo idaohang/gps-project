7723d8a171d66c98cde1d0e794dcfc50db4af601
Kevin James Matzen <kmatzen@cs.cornell.edu>
2013-12-09 22:17:28 -0500

    Updated results.

--- build_geojson_results.m
+++ build_geojson_results.m
@@ -13,14 +13,19 @@ function build_geojson_results ()
         'kalman-time-varying', ...
     };
 
+    process_gains = {0.1, 1, 10};
+
     parfor i=1:length(datasets)
         dataset = datasets{i};
         disp([dataset])
         for j=1:length(gain_modes)
             gain_mode = gain_modes{j};
             disp(['=> ' gain_mode])
-            xhat = filter_dataset(dataset, 'feedback', gain_mode);
-            save_as_geojson(['receiver_paths/' dataset '-' strrep(gain_mode, '-', '_') '.json'], xhat, [dataset '-' gain_mode]);
+            for k=1:length(process_gains)
+                process_gain = process_gains{k};
+                xhat = filter_dataset(dataset, 'feedback', gain_mode, process_gain);
+                save_as_geojson(['receiver_paths/' dataset '-' strrep(gain_mode, '-', '_') '_' num2str(process_gain) '.json'], xhat, [dataset ' ' gain_mode ' ' num2str(process_gain)]);
+            end
         end
         xhat = filter_dataset(dataset, 'absolute');
         save_as_geojson(['receiver_paths/' dataset '-absolute.json'], xhat, [dataset '-absolute']);
